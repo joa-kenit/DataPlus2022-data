abfbd26a2a00d00a1e58bac68130bfcbb41d03b0
tsenanjs <44535881+tsenanjs@users.noreply.github.com>
2022-07-15 15:18:17 -0400

    Add WQI web scraping

--- updatedData/updateData.R
+++ updatedData/updateData.R
@@ -1,53 +1,40 @@
 #Read in web scrapping library
 library(rvest)
-#Open website
-# durhamCityWebsite <- read_html("http://durhamwaterquality.org/index.php")
-# print(durhamCityWebsite %>% html_elements("p") %>% html_text())
-# 
-# #Log into website 
-# search <- html_form(durhamCityWebsite)[[1]]
-# search <- search %>% html_form_set(uname = "jtsenane", passwd = "data+")
-# resp <- html_form_submit(search)
-# durhamCityWebsiteLoggedIn <- read_html(resp)
-# 
-# parameterData <- durhamCityWebsiteLoggedIn%>% html_node("img")
-# parameterData <- durhamCityWebsiteLoggedIn%>% html_node("a")
-# parameterData <- durhamCityWebsiteLoggedIn%>% html_node("option:nth-child(14)")
-# print(parameterData %>% html_text())
-
-
 
 updatedData <- function(){
   tryCatch(
     expr = {
-      #Read data updated May 2022 from github
-      oldData <- read.csv("https://raw.githubusercontent.com/joa-kenit/DataPlus2022-data/main/asites.csv")
-
-      #Read 
-      stations = unique(oldData$Station.Name)
+      #Generate station portion of URL
+      stations = c("EL5.5GC","EL1.9EC","EL5.6EC","EL8.1GC","EL8.5SEC","EL7.1SEC","EL8.6SECUT","EL7.1EC")
       stationsSTR = ""
       for(station in stations){
         stationsSTR = paste0(stationsSTR,"&station%5B%5D=",station)
       }
       
+      #Generate parameter portion of URL
+      oldData <- read.csv("https://raw.githubusercontent.com/joa-kenit/DataPlus2022-data/main/asites.csv")
       parameters = unique(oldData$Parameter)
-      parameters = c("pH","Total Kjeldahl Nitrogen")
       parametersSTR = ""
       for(parameter in parameters){
         parameter = gsub(" ", "+", parameter)
         parametersSTR = paste0(parametersSTR,"&parameter%5B%5D=",parameter)
       }
       
-      durhamCityURL = paste0('http://durhamwaterquality.org/stationQuery1.php?category=name',stationsSTR,'&medium=Water',parametersSTR,'&project%5B%5D=Ambient&beginDate=2003-01-01&endDate=',Sys.Date(),'&p=station&logged_in=1&logged_in=1&qa%5B%5D=3&submit=View+and+Download')
-      durhamCityWebsite <- read_html(durhamCityURL)
-      dataframe = durhamCityWebsite%>% html_node("table.sortable") %>% html_table(header=TRUE)
-      rm(oldData)
+      #Generate full URL
+      paramURL = paste0('http://durhamwaterquality.org/stationQuery1.php?category=name',stationsSTR,'&medium=Water','&parameter%5B%5D=all','&project%5B%5D=Ambient&beginDate=2003-01-01&endDate=',Sys.Date(),'&p=station&logged_in=1&logged_in=1&qa%5B%5D=3&submit=View+and+Download')
+      WQI_URL  = paste0('http://durhamwaterquality.org/wqiQuery1.php?category=name',stationsSTR,'&beginDate=2003-01-01&endDate=',Sys.Date(),'&p=wqi&statistics=year&submit=View+and+Download')
+      
+      #Get website from URL
+      durhamCityWebsite_param <- read_html(paramURL)
+      durhamCityWebsite_WQI <- read_html(WQI_URL)
+      
+      df_param = durhamCityWebsite_param%>% html_node("table.sortable") %>% html_table(header=TRUE)
+      df_WQI = durhamCityWebsite_WQI%>% html_node("table.sortable") %>% html_table(header=TRUE)
       message("Successfully pulled data from the Durham city website.")
-      return(dataframe)
+      return(list(df_param, df_WQI))
     },
     error = function(e){
-      return(oldData)
-      message('Attempted to pull new data from the city of Durham and failed. Now using old data')
+      message('Attempted to pull new data from the city of Durham and failed.')
       print(e)
     },
     warning = function(w){
@@ -60,10 +47,13 @@ updatedData <- function(){
   )    
 }
 
-my_data = updatedData()
+#Run function to get data by parameter and water quality index data
+data_list = updatedData()
+param_data = data_list[1]
+WQI_data = data_list[2]
 
-write.csv(my_data,'updatedData/updatedDurhamData.csv')  
-
-
+#Save data
+write.csv(param_data,'updatedData/updatedParameterDurhamData.csv')  
+write.csv(WQI_data,'updatedData/updatedWQIDurhamData.csv')  
 
       
